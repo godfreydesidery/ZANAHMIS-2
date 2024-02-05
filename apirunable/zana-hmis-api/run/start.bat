82601bf34ba38c41332d577f0395ac1cc2c26242
godfreydesidery <desideryg@gmail.com>
2024-02-05 09:34:58 +0300

    Regular update

---
+++ apirunable/zana-hmis-api/run/start.bat
@@ -0,0 +1,42 @@
+@ECHO OFF
+
+:: BatchGotAdmin
+::-------------------------------------
+REM  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"="
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+::--------------------------------------
+
+::ENTER YOUR CODE BELOW:
+
+ECHO You are about to install application api. We hope that all prerequisites are up and running!
+
+
+
+@echo off&setlocal
+SET folder=%~dp0..\
+echo %folder%
+
+%folder%/resources/winsrvc.exe start
+
+
+
+PAUSE
